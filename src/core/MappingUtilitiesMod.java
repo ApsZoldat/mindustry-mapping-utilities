d3bb267a41ed6114b84d70de909d9b621cc43bcd
ApsZoldat <apszoldat@gmail.com>
2023-04-10 21:35:38 +0300

    Okay i was wrong when saying 'nobody will mind'

--- src/core/MappingUtilitiesMod.java
+++ src/core/MappingUtilitiesMod.java
@@ -21,22 +21,11 @@ public class MappingUtilitiesMod extends Mod {
             try {
                 Vars.maxSchematicSize = 512; // nobody will mind
 
-                // add max/min zoom sliders to settings
-                ui.settings.graphics.sliderPref("maxzoom", 60, 60, 250, 1,
-                i -> Float.toString(Math.round((i / 10f) * 100f) / 100f) + "x");
-                ui.settings.graphics.sliderPref("minzoom", 15, 1, 15, 1,
-                i -> Float.toString(Math.round((i / 10f) * 100f) / 100f) + "x");
-
                 Overrider.override();
             } catch (Exception ex) {
                 Log.err(ex.toString());
                 ui.showException("Mapping Tools Error", ex);
             }
-        });
-
-        Events.run(Trigger.update, () -> {
-            renderer.maxZoom = Core.settings.getInt("maxzoom", 60) / 10f;
-            renderer.minZoom = Core.settings.getInt("minzoom", 15) / 10f;
         });
     }
 
